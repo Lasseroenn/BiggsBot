bee28db35d21af7f82d14656df05a1fd1bc9e9e3
Lasseroenn <lasseroenn@gmail.com>
2018-07-11 15:26:34 +0200

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=biggsbot-Web-Deploy.pubxml -p:Password=mlpsunwLLbkva7Ce1tniPQQCRSce4cScSrJ1SZDlhbGfqMD5FGDDAkLMphwj
+
